7dc12002c05e4777a955a73887df3a6b0812ef3e
wzhao18 <wzhao18.sz@gmail.com>
2024-05-13 20:15:40 -0400

    fixup

--- scripts/eval.py
+++ scripts/eval.py
@@ -15,7 +15,6 @@ def main():
     model_name_or_path = 'OFA-Sys/ofa-large'
     path_to_image = './data/nice-val-5k/215268662.jpg'
 
-    model = OFAModel.from_pretrained(model_name_or_path)
     tokenizer = OFATokenizer.from_pretrained(model_name_or_path)
 
     mean, std = [0.5, 0.5, 0.5], [0.5, 0.5, 0.5]
@@ -27,24 +26,10 @@ def main():
             transforms.Normalize(mean=mean, std=std)
         ])
     
-    txt = " what does the image describe?"
+    txt = "what does the image describe?"
     inputs = tokenizer([txt], return_tensors="pt").input_ids
     img = Image.open(path_to_image)
     patch_img = patch_resize_transform(img).unsqueeze(0)
-
-    # using the generator of fairseq version
-    model = OFAModel.from_pretrained(model_name_or_path, use_cache=True)
-    generator = sequence_generator.SequenceGenerator(
-                        tokenizer=tokenizer,
-                        beam_size=5,
-                        max_len_b=16, 
-                        min_len=0,
-                        no_repeat_ngram_size=3,
-                    )
-    data = {}
-    data["net_input"] = {"input_ids": inputs, 'patch_images': patch_img, 'patch_masks':torch.tensor([True])}
-    gen_output = generator.generate([model], data)
-    gen = [gen_output[i][0]["tokens"] for i in range(len(gen_output))]
 
     # using the generator of huggingface version
     model = OFAModel.from_pretrained(model_name_or_path, use_cache=False)
