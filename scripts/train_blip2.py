eec3a17ba6d1791517f756234f6d336a26bc80f2
XinyanHe <hexy2045@gmail.com>
2024-06-01 23:37:17 +0000

    Delete deepspeed

--- scripts/train_blip2.py
+++ scripts/train_blip2.py
@@ -3,6 +3,7 @@ from tqdm import tqdm
 from PIL import Image
 
 sys.path.append(".")
+# sys.path.append("..")
 
 from transformers import Blip2Processor, Blip2ForConditionalGeneration
 import torch
@@ -10,7 +11,6 @@ from nice.utils import metadata_to_str, set_seed, load_abo_dataset
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from torch.cuda.amp import autocast, GradScaler
-import deepspeed
 
 from nice.blip2 import custom_collate_fn, print_trainable_parameters
 from peft import LoraConfig, get_peft_model
