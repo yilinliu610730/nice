07941d418f0458921d0c18fa8b67255f7b38bc12
XinyanHe <hexy2045@gmail.com>
2024-06-04 18:51:45 +0000

    Clip Finish

--- nice/clip.py
+++ nice/clip.py
@@ -13,6 +13,7 @@ class ClipCapModel(torch.nn.Module):
         self.clip_model = clip_model
         self.gpt_model = gpt_model
         self.projection = torch.nn.Linear(clip_dim, gpt_dim)
+        torch.nn.init.normal_(self.projection.weight, std=0.02)
 
     def forward(self, pixel_values, input_ids):
         image_features = self.clip_model.get_image_features(pixel_values=pixel_values)
@@ -21,7 +22,7 @@ class ClipCapModel(torch.nn.Module):
         input_embeddings = torch.cat((image_features, text_embeddings), dim=1)
         return input_embeddings
 
-def clipcap_infer(clipcap_model, clip_processor, gpt_tokenizer, path_to_image, prompt=None, max_new_tokens=50):
+def clipcap_infer(clipcap_model, clip_processor, gpt_tokenizer, path_to_image, prompt=None, max_new_tokens=256):
     image = Image.open(path_to_image).convert("RGB")
     device = "cuda" if torch.cuda.is_available() else "cpu"
     
@@ -30,44 +31,28 @@ def clipcap_infer(clipcap_model, clip_processor, gpt_tokenizer, path_to_image, p
     image_features = clipcap_model.clip_model.get_image_features(**inputs)
     image_features = clipcap_model.projection(image_features).unsqueeze(1)
     
-    # Set a default prompt if none is provided
-    potential_prompts = [
-        "Describe the key features of this product: ",
-        "What are the main benefits of this product? ",
-        "Provide a detailed description of this product: ",
-        "How can this product be used? ",
-        "What are the advantages of using this product? ",
-        "Explain how this product can solve a problem: ",
-        "Why should a customer buy this product? ",
-        "What makes this product unique? ",
-        "Highlight the unique selling points of this product: ",
-        "List the specifications of this product: ",
-        "What technical details should customers know about this product? "
-    ]
-    potential_prompt = potential_prompts[torch.randint(len(potential_prompts), (1,)).item()]
-    if prompt is None:
-        prompt = potential_prompt
-    else:
-        prompt = prompt + potential_prompt
-    
-    text_inputs = gpt_tokenizer(prompt, return_tensors="pt").input_ids.to(device)
-    text_attention_mask = gpt_tokenizer(prompt, return_tensors="pt").attention_mask.to(device)
+    # Set the single prompt
+    prompt = "What is the description of the product?" if prompt is None else prompt
+    text_inputs = gpt_tokenizer(prompt, return_tensors="pt", truncation=True, max_length=1024).input_ids.to(device)
+    text_attention_mask = gpt_tokenizer(prompt, return_tensors="pt", truncation=True, max_length=1024).attention_mask.to(device)
     
     # Generate attention mask for the concatenated input
     batch_size, seq_length = text_inputs.shape
     image_attention_mask = torch.ones((batch_size, 1), device=device)
     attention_mask = torch.cat((image_attention_mask, text_attention_mask), dim=1)
-
+    
     # Generate caption with adjusted parameters
     input_embeds = torch.cat((image_features, clipcap_model.gpt_model.transformer.wte(text_inputs)), dim=1)
+    max_length = input_embeds.size(1) + max_new_tokens
+
     generated_ids = clipcap_model.gpt_model.generate(
-        inputs_embeds=input_embeds, 
-        attention_mask=attention_mask, 
-        max_new_tokens=max_new_tokens, 
+        inputs_embeds=input_embeds,
+        attention_mask=attention_mask,
+        max_length=256,
         pad_token_id=gpt_tokenizer.eos_token_id,
         temperature=1.0,  # Adjust temperature for more randomness
-        top_k=50,         # Consider only the top 50 tokens
-        top_p=0.9,        # Use nucleus sampling
+        top_k=30,         # Consider only the top 30 tokens
+        top_p=0.85,       # Use nucleus sampling
         repetition_penalty=2.0,  # Apply repetition penalty
         num_return_sequences=1,
         early_stopping=True
@@ -77,13 +62,6 @@ def clipcap_infer(clipcap_model, clip_processor, gpt_tokenizer, path_to_image, p
     
     return caption
 
-def truncate_or_pad(sequence, max_length, pad_token_id):
-    if sequence.size(1) > max_length:
-        return sequence[:, :max_length]
-    else:
-        padding = torch.full((sequence.size(0), max_length - sequence.size(1)), pad_token_id, dtype=sequence.dtype, device=sequence.device)
-        return torch.cat([sequence, padding], dim=1)
-
 def custom_collate_fn(batch, clip_processor, gpt_tokenizer, max_length=77):
     input_images = []
     labels_list = []
@@ -91,6 +69,7 @@ def custom_collate_fn(batch, clip_processor, gpt_tokenizer, max_length=77):
     bullet_points_gt_list = []
     main_image_id_list = []
     path_to_image_list = []
+    prompt_inputs_list = []
 
     for image_data in batch:
         main_image_id = image_data["main_image_id"]
@@ -98,6 +77,8 @@ def custom_collate_fn(batch, clip_processor, gpt_tokenizer, max_length=77):
         bullet_points = image_data["bullet_points"]
         meta_data = image_data["metadata"]
         meta_str = metadata_to_str(meta_data)
+        prefix = 'What is the item description?'
+        prompt = prefix + " Metadata: " + meta_str
 
         bullet_points_gt = "; ".join([bullet_point["value"] for bullet_point in bullet_points])
 
@@ -106,28 +87,36 @@ def custom_collate_fn(batch, clip_processor, gpt_tokenizer, max_length=77):
         inputs = clip_processor(images=image, return_tensors="pt")
         input_images.append(inputs.pixel_values)
 
+        prompt_inputs = gpt_tokenizer(
+            prompt, 
+            return_tensors="pt", 
+            padding="max_length", 
+            truncation=True, 
+            max_length=76
+        ).input_ids
+        
         labels = gpt_tokenizer(
-            text=bullet_points_gt,
-            return_tensors="pt",
-            padding="max_length",
-            truncation=True,
+            bullet_points_gt, 
+            return_tensors="pt", 
+            padding="max_length", 
+            truncation=True, 
             max_length=max_length
         ).input_ids
-
+        
         labels_list.append(labels)
         meta_str_list.append(meta_str)
+        prompt_inputs_list.append(prompt_inputs)
         bullet_points_gt_list.append(bullet_points_gt)
         main_image_id_list.append(main_image_id)
         path_to_image_list.append(path_to_image)
 
-    pad_token_id = gpt_tokenizer.pad_token_id
-
-    # Ensure labels are truncated or padded to max_length
-    labels = torch.cat([truncate_or_pad(lbls, max_length, pad_token_id) for lbls in labels_list], dim=0)
     input_images = torch.cat(input_images, dim=0)
+    labels = torch.cat(labels_list, dim=0)
+    prompt_inputs = torch.cat(prompt_inputs_list)
 
     return {
         "pixel_values": input_images,
+        "prompt_inputs": prompt_inputs,
         "labels": labels,
         "meta_str": meta_str_list,
         "bullet_points_gt": bullet_points_gt_list,
@@ -155,55 +144,63 @@ def main():
     gpt_model_name = "gpt2"
 
     clip_processor = CLIPProcessor.from_pretrained(clip_model_name)
-    clip_model = CLIPModel.from_pretrained("pretrained_clip_model_lora").to(device)
+    clip_model = CLIPModel.from_pretrained("pretrained_clip_model").to(device)
 
     gpt_tokenizer = GPT2Tokenizer.from_pretrained(gpt_model_name)
-    gpt_model = GPT2LMHeadModel.from_pretrained(gpt_model_name).to(device)
+    gpt_model = GPT2LMHeadModel.from_pretrained("pretrained_gpt_model_clip").to(device)
     clipcap_model = ClipCapModel(clip_model, gpt_model).to(device)
 
-    # Set pad token if it doesn't exist
-    if gpt_tokenizer.pad_token is None:
-        gpt_tokenizer.pad_token = gpt_tokenizer.eos_token
+    # # Set pad token if it doesn't exist
+    # if gpt_tokenizer.pad_token is None:
+    #     gpt_tokenizer.pad_token = gpt_tokenizer.eos_token
 
-    optimizer = AdamW(clipcap_model.parameters(), lr=5e-5)
+    # optimizer = AdamW(clipcap_model.parameters(), lr=5e-5)
 
-    print_trainable_parameters(clipcap_model)
+    # print_trainable_parameters(clipcap_model)
 
-    clipcap_model.train()
-    epochs = 1
-    BATCH_SIZE = 1  # Reduce batch size
-    accumulation_steps = 4  # Number of steps to accumulate gradients
+    # clipcap_model.train()
+    # epochs = 3
+    # BATCH_SIZE = 4  # Reduce batch size
+    # accumulation_steps = 4  # Number of steps to accumulate gradients
 
-    collate_fn = lambda batch: custom_collate_fn(batch, clip_processor, gpt_tokenizer)
+    # collate_fn = lambda batch: custom_collate_fn(batch, clip_processor, gpt_tokenizer)
 
-    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
+    # train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=collate_fn)
 
-    for epoch in range(epochs):
-        print(f"Epoch {epoch+1}/{epochs}")
-        optimizer.zero_grad()
-        for i, batch in enumerate(tqdm(train_loader)):
-            input_images = batch["pixel_values"].to(device)
-            labels = batch["labels"].to(device)
+    # for epoch in range(epochs):
+    #     print(f"Epoch {epoch+1}/{epochs}")
+    #     optimizer.zero_grad()
+    #     for i, batch in enumerate(tqdm(train_loader)):
+    #         input_images = batch["pixel_values"].to(device)
+    #         labels = batch["labels"].to(device)
+    #         text_inputs = batch["prompt_inputs"].to(device)
 
-            input_embeds = clipcap_model(input_images, labels[:, :-1])
+            # # Set the prompt with metadata
+            # prompt = f"What is the description of the product? Metadata: {batch['meta_str'][0]}"
+            # text_inputs = gpt_tokenizer(
+            #     prompt, 
+            #     return_tensors="pt", 
+            #     padding="max_length", 
+            #     truncation=True, 
+            #     max_length=76
+            # ).input_ids.to(device)
 
-            # Ensure the batch size matches for input_embeds and labels
-            assert input_embeds.size(1) == labels.size(1), "Input embeddings and labels must match in sequence length"
+    #         input_embeds = clipcap_model(input_images, text_inputs)
 
-            outputs = clipcap_model.gpt_model(inputs_embeds=input_embeds, labels=labels)
-            loss = outputs.loss
-            loss = loss / accumulation_steps  # Scale loss
-            loss.backward()
+    #         outputs = clipcap_model.gpt_model(inputs_embeds=input_embeds, labels=labels)
+    #         loss = outputs.loss
+    #         loss = loss / accumulation_steps  # Scale loss
+    #         loss.backward()
 
-            if (i + 1) % accumulation_steps == 0:
-                optimizer.step()
-                optimizer.zero_grad()
+    #         if (i + 1) % accumulation_steps == 0:
+    #             optimizer.step()
+    #             optimizer.zero_grad()
 
-            tqdm.write(f"Loss: {loss.item()}")
+    #         tqdm.write(f"Loss: {loss.item()}")
 
-    # Save the model
-    clipcap_model.gpt_model.save_pretrained("pretrained_gpt_model_clip")
-    clipcap_model.clip_model.save_pretrained("pretrained_clip_model")
+    # # Save the model
+    # clipcap_model.gpt_model.save_pretrained("pretrained_gpt_model_clip")
+    # clipcap_model.clip_model.save_pretrained("pretrained_clip_model")
 
     # Evaluation
     clipcap_model.eval()
@@ -214,20 +211,16 @@ def main():
         path_to_image = image_data["path"]
         bullet_points = image_data["bullet_points"]
         meta_data = image_data["metadata"]
-        meta_str = metadata_to_str(meta_data)
+        meta_str = metadata_to_str(meta_data)[:2048]
 
         bullet_points_gt = "; ".join([bullet_point["value"] for bullet_point in bullet_points])
-        clip_caption = clipcap_infer(clipcap_model, clip_processor, gpt_tokenizer, path_to_image)
-        meta_str = meta_str[:2048]
         clip_prompt = " Metadata: " + meta_str
         clip_caption_with_meta = clipcap_infer(clipcap_model, clip_processor, gpt_tokenizer, path_to_image, prompt=clip_prompt, max_new_tokens=256)
 
-        clip_pred.append((main_image_id, path_to_image, bullet_points_gt, clip_caption, clip_caption_with_meta, meta_str))
-        print("clip_caption: ", clip_caption)
-        print("\n")
+        clip_pred.append((main_image_id, path_to_image, bullet_points_gt, clip_caption_with_meta, meta_str))
         print("clip_caption_with_meta: ", clip_caption_with_meta)
 
-    out_df = pd.DataFrame(clip_pred, columns=["main_image_id", "path_to_image", "bullet_points_gt", "clip_caption", "clip_caption_with_meta", "metadata"])
+    out_df = pd.DataFrame(clip_pred, columns=["main_image_id", "path_to_image", "bullet_points_gt", "clip_caption_with_meta", "metadata"])
     out_df.to_csv("pred_clip.csv", index=False)
 
 if __name__ == '__main__':
